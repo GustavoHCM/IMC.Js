193bd5c920e307bfe287bed3e99b5d58b291a64a
GustavoHCM <144824938+GustavoHCM@users.noreply.github.com>
2024-11-08 11:10:23 -0300

    Add files via upload

---
+++ imc.js
@@ -0,0 +1,12 @@
+let peso = parseFloat(prompt("informe o seu peso:"));
+let altura=parseFloat(prompt("informe sua altura"));
+
+if( isNaN(peso) || ispn(altura)||peso<=0||altura){
+    document.write("porfavor informar valores validos!!!");}
+    else{
+            let Imc=(peso/altura*altura)
+   document.write("o seu imc é igual a;")
+   if(imc>=18 && imc <249){
+    document.write("peso ideal")
+   }else if (imc>249 && imc<=28){
+    }}
