420fa61fdaa8fc031d72a3e8713a88e8bbed3ef2
GustavoHCM <144824938+GustavoHCM@users.noreply.github.com>
2024-11-11 09:53:10 -0300

    Add files via upload

--- imc.js
+++ imc.js
@@ -4,7 +4,7 @@ let altura=parseFloat(prompt("informe sua altura"));
 if( isNaN(peso) || ispn(altura)||peso<=0||altura){
     document.write("porfavor informar valores validos!!!");}
     else{
-            let Imc=(peso/altura*altura)
+            let imc=(peso/altura*altura)
    document.write("o seu imc é igual a;")
    if(imc>=18 && imc <249){
     document.write("peso ideal")
